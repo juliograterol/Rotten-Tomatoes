fa0d19fe38d69bf088cbd3aa667b7f17e6b27218
Julio Graterol <juliograterolb@gmail.com>
2023-12-10 17:29:12 -0400

    Added Page Folder and deleted Tabs

---
+++ src/app/pages/discover/discover.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ExploreContainerComponentModule } from 'src/app/explore-container/explore-container.module';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DiscoverPageRoutingModule } from './discover-routing.module';
+
+import { DiscoverPage } from './discover.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExploreContainerComponentModule,
+    DiscoverPageRoutingModule,
+  ],
+  declarations: [DiscoverPage],
+})
+export class DiscoverPageModule {}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,29 +8,44 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'discover',
+        loadChildren: () =>
+          import('../pages/discover/discover.module').then(
+            (m) => m.DiscoverPageModule
+          ),
+      },
+
+      {
+        path: 'movies',
+        loadChildren: () =>
+          import('../pages/movies/movies.module').then(
+            (m) => m.MoviesPageModule
+          ),
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'tv',
+        loadChildren: () =>
+          import('../pages/tv/tv.module').then((m) => m.TvPageModule),
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'profile',
+        loadChildren: () =>
+          import('../pages/profile/profile.module').then(
+            (m) => m.ProfilePageModule
+          ),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        redirectTo: '/tabs/discover',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    redirectTo: '/tabs/discover',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
